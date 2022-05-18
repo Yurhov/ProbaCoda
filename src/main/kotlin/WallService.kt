3c6bd0561007c3a4c94e2da94739f206ca07be60
Юрий Орехов <yurhov@yandex.ru>
2022-05-18 22:52:17 +0300

    6

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,11 +1,11 @@
-object WallService {
-    var posts = emptyArray<Post>()
-    private var postId: Int = 1
-    fun add(post: Post): Post {
-        val newPost = post.copy(id = postId)
-        posts += newPost
-        postId++
-        println(newPost)
-        return posts.last()
-    }
-}
+//object WallService {
+//    var posts = emptyArray<Post>()
+//    private var postId: Int = 1
+//    fun add(post: Post): Post {
+//        val newPost = post.copy(id = postId)
+//        posts += newPost
+//        postId++
+//        println(newPost)
+//        return posts.last()
+//    }
+//}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
-fun main() {
-    WallService.add(Post( 0,"про", "еоу"))
-    WallService.add(Post( 0,"кен", "ах2"))
-    WallService.add(Post( 0,"rom", "Ok"))
-    WallService.add(Post())
-    println( WallService.posts[1].activities[0])
-}
+//fun main() {
+//    WallService.add(Post( 0,"про", "еоу"))
+//    WallService.add(Post( 0,"кен", "ах2"))
+//    WallService.add(Post( 0,"rom", "Ok"))
+//    WallService.add(Post())
+//    println( WallService.posts[1].activities[0])
+//}
