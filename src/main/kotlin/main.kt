85149497e7e3f68301e197f86a1cd6848162e408
Yurhov <yurhov@yandex.ru>
2022-06-18 19:11:23 +0300

    коллекции

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,44 @@
-//fun main() {
-//    WallService.add(Post( 0,"про", "еоу"))
-//    WallService.add(Post( 0,"кен", "ах2"))
-//    WallService.add(Post( 0,"rom", "Ok"))
-//    WallService.add(Post())
-//    println( WallService.posts[1].activities[0])
-//}
+import kotlin.math.max
+
+fun main() {
+    val collection = mutableMapOf("x" to 1, "y" to 2, "z" to 3)
+    for ((key, value) in collection) {
+        println("Key is $key, value is $value")
+    }
+    println(collection.containsKey("x"))
+    println(collection.containsValue(2))
+    println(collection.get("y"))
+    println(collection["a"])
+    println(collection.getValue("z"))
+    println(collection.entries)
+    println(collection.keys)
+    println(collection.values)
+    println(collection.size)
+    println(collection.isEmpty())
+
+//    collection.put("a", 4)
+//    collection["b"] = 5
+//    println(collection.entries)
+//
+//    val otherCollection = mapOf("c" to 6, "d" to 7)
+//    collection.putAll(otherCollection)
+//    println(collection.entries)
+//
+//    collection.remove("x")
+//    collection.remove("z", 4)
+//    println(collection.entries)
+
+    val collectionList = collection.toList()
+    println(collectionList)
+
+    val collectionSet = collection.values.toSet()
+    println(collectionSet)
+
+//   collection.clear()
+    println(collection.isEmpty())
+    println(collection.values.random())
+    println(collection.values.maxOrNull())
+    println(collection.keys.minOrNull())
+}
+
+
